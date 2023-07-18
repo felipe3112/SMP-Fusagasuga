10a941d6f557ff1d44ddc33d9991a57a169db8a6
felipe3112 <mfzn311298@gmail.com>
2023-07-18 09:05:44 -0500

    fix(header,icons__social--nav)

--- js/burguer.js
+++ js/burguer.js
@@ -1,26 +1,32 @@
+'use strict'
+
 let boton = document.getElementById("icono");
 let links = document.getElementById("ul-lista");
 let contador = 0;
 
-const icono = document.getElementById('icono')
+const icono = document.getElementById('icono');
 
-icono.addEventListener('pointerdown', function() {
-    this.classList.toggle('activo');
-  });
+icono.addEventListener('pointerdown', function () {
+    this
+        .classList
+        .toggle('activo');
+});
 
-boton.addEventListener("pointerdown", function(){
-    if(contador == 0){
+boton.addEventListener("pointerdown", function () {
+    if (contador == 0) {
         links.className = ('ul-lista dos');
-        contador = 1; 
-    }else{
-        links.classList.remove('dos');
+        contador = 1;
+    } else {
+        links
+            .classList
+            .remove('dos');
         links.className = ('ul-lista uno')
         contador = 0;
     }
 })
 
-window.addEventListener('resize', function(){
-    if(this.screen.width > 800){
+window.addEventListener('resize', function () {
+    if (this.screen.width > 800) {
         contador = 0;
         links.classList.remove('dos');
         links.className = ('ul-lista uno')
