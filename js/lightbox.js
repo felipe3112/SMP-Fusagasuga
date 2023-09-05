843b3d47af7be2950331aa0ad543a61a84ed10c5
felipe3112 <mfzn311298@gmail.com>
2023-09-05 10:45:07 -0500

    05/09 10:45am

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,21 +1,21 @@
 'use strict'
 
 //constantes y variables
-const imgList  = document.querySelectorAll('.img')
-const lightbox = document.querySelector('.lightbox')
-const grande   = document.querySelector('.grande')
-const closeBtn = document.querySelector('.close')
+const imgList  = document.querySelectorAll('.img');
+const lightbox = document.querySelector('.lightbox');
+const grande   = document.querySelector('.grande');
+const closeBtn = document.querySelector('.close');
 //Definir funciones 
-const closeBtnHandler = () => lightbox.classList.remove('isActive')
+const closeBtnHandler = () => lightbox.classList.remove('isActive');
 const imgListHandler = index =>{
-    lightbox.classList.add('isActive')
+    lightbox.classList.add('isActive');
     grande.src = imgList[index].src
 }
 
 imgList.forEach((eachImg , index)=>{
     imgList[index].addEventListener('click', ()=>{
-        imgListHandler(index)
+        imgListHandler(index);
     })
 })
 
-closeBtn.addEventListener('click', closeBtnHandler)
+closeBtn.addEventListener('click', closeBtnHandler);
--- js/slider.js
+++ js/slider.js
@@ -17,3 +17,5 @@
         sliders[currentSliderIndex].classList.add('slider__body--show');
     }
 })();
+
+
