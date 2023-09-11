7a7547f26d592614b09a39ebc8f1d5e2c2421f63
felipe3112 <mfzn311298@gmail.com>
2023-09-11 09:55:17 -0500

    blur myv, & 11/09

--- js/navbar.js
+++ js/navbar.js
@@ -26,3 +26,19 @@ window.addEventListener("scroll", () => {
             .remove("active");
     }
 })
+
+let animado = document.querySelectorAll('.animado');
+
+function mostrarScroll(){
+    let scrollTop = document.documentElement.scrollTop;
+
+    for (let i = 0; i < animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+        if (alturaAnimado - 450 < scrollTop) {
+            animado[i].style.opacity =1;
+            animado[i].classList.add('mostrarArriba');
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll);
