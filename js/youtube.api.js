ddb3cb1629119c3f583d19fe59731115f3ba8ef0
felipe3112 <mfzn311298@gmail.com>
2023-09-27 12:18:54 -0500

    27

--- js/youtube.api.js
+++ js/youtube.api.js
@@ -11,6 +11,9 @@ let videoNosotros;
 let videoGaleria1;
 let videoGaleria2;
 let videoGaleria3;
+let videoGaleria4;
+let videoGaleria5;
+let videoGaleria6;
 
 // Función llamada cuando se carga la API de YouTube
 function onYouTubeIframeAPIReady() {
@@ -45,8 +48,9 @@ function onYouTubeIframeAPIReady() {
       onReady: onPlayerReady
     }
   });
+
   videoGaleria1 = new YT.Player('player-galeria1', {
-    videoId: 'kTOGVFPQ4s8',
+    videoId: 'Jvf-9nEyBDA',
     playerVars: {
       autoplay: 0,
       hd: 1,
@@ -61,7 +65,7 @@ function onYouTubeIframeAPIReady() {
     }
   });
   videoGaleria2 = new YT.Player('player-galeria2', {
-    videoId: 'kTOGVFPQ4s8',
+    videoId: 'RwNL7BjPWyM',
     playerVars: {
       autoplay: 0,
       hd: 1,
@@ -76,7 +80,52 @@ function onYouTubeIframeAPIReady() {
     }
   });
   videoGaleria3 = new YT.Player('player-galeria3', {
-    videoId: 'kTOGVFPQ4s8',
+    videoId: 'kLQK4yNjfg8',
+    playerVars: {
+      autoplay: 0,
+      hd: 1,
+      controls: 1, // Controles
+      modestbranding: 1,
+      showinfo: 0,
+      rel: 0,
+      enablejsapi: 1 // Habilitar la API de JavaScript
+    },
+    events: {
+      onReady: onPlayerReady
+    }
+  });
+  videoGaleria4 = new YT.Player('player-galeria4', {
+    videoId: 'OkdzV10KPIY',
+    playerVars: {
+      autoplay: 0,
+      hd: 1,
+      controls: 1, // Controles
+      modestbranding: 1,
+      showinfo: 0,
+      rel: 0,
+      enablejsapi: 1 // Habilitar la API de JavaScript
+    },
+    events: {
+      onReady: onPlayerReady
+    }
+  });
+  videoGaleria5 = new YT.Player('player-galeria5', {
+    videoId: '3m1Runnc9X4',
+    playerVars: {
+      autoplay: 0,
+      hd: 1,
+      controls: 1, // Controles
+      modestbranding: 1,
+      showinfo: 0,
+      rel: 0,
+      enablejsapi: 1 // Habilitar la API de JavaScript
+    },
+    events: {
+      onReady: onPlayerReady
+    }
+  });
+  videoGaleria6 = new YT.Player('player-galeria6', {
+    videoId: 'ZIQsbkc7j4o',
     playerVars: {
       autoplay: 0,
       hd: 1,
